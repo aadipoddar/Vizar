2f5347c44dcfaea821aeadbc4f034047f7b3a616
Aadi Poddar <aadipoddarmail@gmail.com>
2025-10-12 15:55:51 +0530

    Project Setup

---
+++ Vizar/Vizar.Shared/Services/IFormFactor.cs
@@ -0,0 +1,8 @@
+namespace Vizar.Shared.Services
+{
+    public interface IFormFactor
+    {
+        public string GetFormFactor();
+        public string GetPlatform();
+    }
+}
---
+++ Vizar/Vizar/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Vizar.Services;
+using Vizar.Shared.Services;
+
+namespace Vizar
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            // Add device-specific services used by the Vizar.Shared project
+            builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
